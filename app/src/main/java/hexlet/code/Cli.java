b49f9df887321455797e5417417515d530462c3b
IvanTlesov <tlesov23@gmail.com>
2025-11-10 16:08:58 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,12 +2,15 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String userName;
+    private static String userName;
     public static void startGame() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         userName = sc.nextLine();
         System.out.println("Hello, " + userName + "!");
+    }
+    public static String getName() {
+        return userName;
     }
 }
