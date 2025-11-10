d8d5248fa505858a946ab13428f80bf25e13bacf
IvanTlesov <tlesov23@gmail.com>
2025-11-10 16:11:19 +0300

    update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,11 +1,16 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 import java.util.StringJoiner;
 
 public class Progression {
+    static final int MAXSTART = 30;
+    static final int MAXSTEP = 10;
+    static final int MINPROGRESSIONLENGTH = 5;
     public static String[][] generateQA() {
-        String[][] questionAnswer = new String[3][2];
+        String[][] questionAnswer = new String[Engine.MAXROUNDCOUNTER][2];
         for (int i = 0; i < questionAnswer.length; i++) {
             String[] qa = generateProgression();
             questionAnswer[i][0] = qa[0];
@@ -17,9 +22,9 @@ public class Progression {
     public static String[] generateProgression() {
         Random random = new Random();
         String[] qas = new String[2];
-        int startElement = random.nextInt(30);
-        int step = random.nextInt(10);
-        int length = random.nextInt(5) + 5;
+        int startElement = random.nextInt(MAXSTART);
+        int step = random.nextInt(MAXSTEP);
+        int length = random.nextInt(MINPROGRESSIONLENGTH) + MINPROGRESSIONLENGTH;
         int invisibleElement = random.nextInt(length);
         StringJoiner progression = new StringJoiner(" ");
         for (int i = 0; i < length; i++) {
