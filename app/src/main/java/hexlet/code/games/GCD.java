a4b851b985311e91e1fa828adca49941bd5e7d3a
IvanTlesov <tlesov23@gmail.com>
2025-11-10 16:10:47 +0300

    update GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,14 +1,16 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 
 public class GCD {
     public static String[][] generateQA() {
         Random random = new Random();
-        String[][] questionAnswer = new String[3][2];
+        String[][] questionAnswer = new String[Engine.MAXROUNDCOUNTER][2];
         for (int i = 0; i < questionAnswer.length; i++) {
-            int firstNumber = random.nextInt(30);
-            int secondNumber = random.nextInt(30);
+            int firstNumber = random.nextInt(Engine.MAXRANDOMNUMBER);
+            int secondNumber = random.nextInt(Engine.MAXRANDOMNUMBER);
             questionAnswer[i][0] = firstNumber + " " + secondNumber;
             questionAnswer[i][1] = String.valueOf(evklidRespect(firstNumber, secondNumber));
         }
