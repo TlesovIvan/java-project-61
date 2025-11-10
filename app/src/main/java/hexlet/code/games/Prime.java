6d2ad5d3f3e3e92b90825701041af01bd366263e
IvanTlesov <tlesov23@gmail.com>
2025-11-10 15:07:30 +0300

    add Prime.java

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,33 @@
+package hexlet.code.games;
+
+import java.util.Random;
+
+public class Prime {
+    public static String[][] generateQA() {
+        Random random = new Random();
+        String[][] questionAnswer = new String[3][2];
+        for (int i = 0; i < questionAnswer.length; i++) {
+            questionAnswer[i][0] = String.valueOf(random.nextInt(100));
+            questionAnswer[i][1] = isPrime(Integer.parseInt(questionAnswer[i][0]));
+        }
+        return questionAnswer;
+    }
+
+    public static String isPrime(int number) {
+        if (number < 2) {
+            return "no";
+        }
+        if (number == 2) {
+            return "yes";
+        }
+        if (number % 2 == 0) {
+            return "no";
+        }
+        for (int i = 3; i < (int) Math.sqrt(number); i+=2) {
+            if (number % i == 0) {
+                return "no";
+            }
+        }
+        return "yes";
+    }
+}
