4710f641f4158a17d4e9a326ffb1d2cb8b40fd04
IvanTlesov <tlesov23@gmail.com>
2025-11-10 16:10:20 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,13 +1,15 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 
 public class Even {
     public static String[][] generateQA() {
-        String[][] questionAnswer = new String[3][2];
+        String[][] questionAnswer = new String[Engine.MAXROUNDCOUNTER][2];
         Random random = new Random();
         for (int i = 0; i < questionAnswer.length; i++) {
-            questionAnswer[i][0] = String.valueOf(random.nextInt(100));
+            questionAnswer[i][0] = String.valueOf(random.nextInt(Engine.MAXRANDOMNUMBER));
             questionAnswer[i][1] = isEven(Integer.parseInt(questionAnswer[i][0]));
         }
         return questionAnswer;
