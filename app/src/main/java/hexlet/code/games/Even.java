9545caa51c5636f6715cf359a263bdff18a68117
IvanTlesov <tlesov23@gmail.com>
2025-11-10 15:05:40 +0300

    add Even.java

---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,19 @@
+package hexlet.code.games;
+
+import java.util.Random;
+
+public class Even {
+    public static String[][] generateQA() {
+        String[][] questionAnswer = new String[3][2];
+        Random random = new Random();
+        for (int i = 0; i < questionAnswer.length; i++) {
+            questionAnswer[i][0] = String.valueOf(random.nextInt(100));
+            questionAnswer[i][1] = isEven(Integer.parseInt(questionAnswer[i][0]));
+        }
+        return questionAnswer;
+    }
+
+    public static String isEven(int number) {
+        return number % 2 == 0 ? "yes" : "no";
+    }
+}
