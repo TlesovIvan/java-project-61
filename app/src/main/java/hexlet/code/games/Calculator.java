bf859532f0d7447b72de251fca862a9e3b8abaa4
IvanTlesov <tlesov23@gmail.com>
2025-11-10 16:10:00 +0300

    update Calculator.java

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -1,10 +1,13 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 
 public class Calculator {
+    static final int COUNTSIGN = 3;
     public static String[][] generateQA() {
-        String[][] questionAnswer = new String[3][2];
+        String[][] questionAnswer = new String[Engine.MAXROUNDCOUNTER][2];
         for (int i = 0; i < questionAnswer.length; i++) {
             String[] qa = generateSign();
             questionAnswer[i][0] = qa[0];
@@ -16,9 +19,9 @@ public class Calculator {
     public static String[] generateSign() {
         String[] qas = new String[2];
         Random random = new Random();
-        int firstNumber = random.nextInt(150);
-        int secondNumber = random.nextInt(150);
-        int randomNumberForSign = random.nextInt(3) + 1;
+        int firstNumber = random.nextInt(Engine.MAXRANDOMNUMBER);
+        int secondNumber = random.nextInt(Engine.MAXRANDOMNUMBER);
+        int randomNumberForSign = random.nextInt(COUNTSIGN) + 1;
         switch (randomNumberForSign) {
             case 1:
                 qas[0] = firstNumber + " + " + secondNumber;
@@ -31,6 +34,8 @@ public class Calculator {
             case 3:
                 qas[0] = firstNumber + " * " + secondNumber;
                 qas[1] = String.valueOf(firstNumber * secondNumber);
+                break;
+            default:
                 break;
         }
         return qas;
